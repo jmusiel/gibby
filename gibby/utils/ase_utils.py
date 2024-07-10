74058bbea8ce25ac37561d4e1d5ee4e3d75d0751
jmusiel <73840022+jmusiel@users.noreply.github.com>
2024-07-10 18:33:07 +0000

    add get analytical hessian function

--- gibby/utils/ase_utils.py
+++ gibby/utils/ase_utils.py
@@ -36,3 +36,22 @@ def get_hessian(atoms, run_dir=None):
     if run_dir is not None:
         os.chdir(cwd)
     return hessian
+
+def get_analytical_hessian(atoms):
+    """
+        Get the analytical hessian from the OCPCalWrapper and an OCP model which saves the hessian during forward.
+    """
+    free_indices = [i for i in range(len(atoms)) if not i in atoms.constraints[0].index]
+    full_hessian = atoms.calc.extract_hessian(atoms)
+    hessian = full_hessian.reshape((len(atoms), 3, len(atoms), 3))
+    hessian = hessian[free_indices, :, :, :]
+    hessian = hessian[:, :, free_indices, :]
+    hessian = hessian.reshape((len(free_indices)*3, len(free_indices)*3))
+
+    # # alternative approach to get the hessian, gets the same thing
+    # hessian_free_indices = np.array([[ind*3, ind*3+1, ind*3+2] for ind in free_indices]).flatten()
+    # hessian_alt = full_hessian.reshape((len(atoms)* 3, len(atoms)* 3))
+    # hessian_alt = hessian_alt[hessian_free_indices,:]
+    # hessian_alt = hessian_alt[:,hessian_free_indices]
+
+    return hessian
